685cdd43526615e2a84505884c92e782297a022f
Gianfranco Elli <gianelli99@hotmail.com>
2025-04-15 00:32:59 -0300

    Add just a few components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Textarea } from "@/components/ui/textarea";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -8,10 +14,26 @@ export const Route = createFileRoute("/")({
 function App() {
   return (
     <div>
-      <header className="text-white text-3xl">
-        <h1>.*ignore playground</h1>
+      <header>
+        <h1 className="text-3xl">.ignore playground</h1>
+        <p>
+          Edit the ignore file on the left and see which files would be ignored
+          or tracked
+        </p>
       </header>
-      <Button variant="secondary">Click me</Button>
+      <main>
+        <ResizablePanelGroup direction="horizontal" className="border-t">
+          <ResizablePanel>
+            <h2>.gitignore</h2>
+            <Textarea className="font-mono" />
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>
+            <h2>File Structure</h2>
+            {/* Tree view */}
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
     </div>
   );
 }
